485d55c619a84d02fe91f1ef41e2da87b5dfac0d
zhao-feifei <398983914@qq.com>
2021-11-26 19:45:44 +0800

    基本结构搭建完成

---
+++ src/compile.js
@@ -0,0 +1,5 @@
+//负责模板解析的类
+class Compile {
+  //接收两个参数,  模板和vm实例
+  constructor(el, vm) {}
+}
---
+++ src/vue.js
@@ -0,0 +1,12 @@
+class Vue {
+  constructor(options = {}) {
+    // 将传过来的数据绑定给vm实例
+    this.$el = options.el
+    this.$data = options.data
+    // 如果指定了el,则对el进行解析
+    if (this.$el) {
+      // Compile负责解析模板
+      new Compile(this.$el, this) //这里将  模板,  vm实例  传递过去
+    }
+  }
+}
