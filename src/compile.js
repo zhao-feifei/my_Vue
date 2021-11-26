317b137bbb630541c7d5d0c3e756dcbb71fd2b18
zhao-feifei <398983914@qq.com>
2021-11-26 20:59:24 +0800

    将节点放入到fragment中

--- src/compile.js
+++ src/compile.js
@@ -1,5 +1,30 @@
 //负责模板解析的类
 class Compile {
-  //接收两个参数,  模板和vm实例
-  constructor(el, vm) {}
+  constructor(el, vm) {
+    // el: new Vue传递的选择器
+    this.el = typeof el === 'string' ? document.querySelector(el) : el
+    //vm: new 的Vue实例
+    this.vm = vm
+    //编译模板
+    if (this.el) {
+      //1.将el中所有的子节点放入内存
+      let fragment = this.node2fragment(this.el)
+      //2.在内存中编译fragment
+      //3.把fragment一次性添加到页面
+    }
+  }
+
+  //重要方法
+  node2fragment(node) {
+    let fragment = document.createDocumentFragment()
+    //把el中所有的子节点全部添加到文档碎片中
+    let childNodes = node.childNodes
+    this.toArray(childNodes).forEach((node) => {
+      fragment.appendChild(node)
+    })
+  }
+  //工具方法
+  toArray(likeArray) {
+    return [].slice.call(likeArray)
+  }
 }
