b7669c5b87fa9c643f5a5b93afd478ec36e0df8e
zhao-feifei <398983914@qq.com>
2021-11-27 11:38:56 +0800

    解析fragment完成

--- src/compile.js
+++ src/compile.js
@@ -10,21 +10,65 @@ class Compile {
       //1.将el中所有的子节点放入内存
       let fragment = this.node2fragment(this.el)
       //2.在内存中编译fragment
+      this.compile(fragment)
       //3.把fragment一次性添加到页面
     }
   }
 
-  //重要方法
+  /*---------------------------------核心方法-------------------------------*/
+
+  // 将node转换为fragment
   node2fragment(node) {
     let fragment = document.createDocumentFragment()
     //把el中所有的子节点全部添加到文档碎片中
     let childNodes = node.childNodes
+
     this.toArray(childNodes).forEach((node) => {
       fragment.appendChild(node)
     })
+    return fragment
   }
-  //工具方法
+  //在内存中编译文档碎片
+  compile(fragment) {
+    let childNodes = fragment.childNodes
+    this.toArray(childNodes).forEach((node) => {
+      //如果是文本节点，解析插值表达式
+
+      if (this.isElementNode(node)) {
+        //如果是元素则解析指令
+        this.compileElement(node)
+      }
+      if (this.isTextNode(node)) {
+        //如果是文本节点，解析插值表达式
+        this.compileText(node)
+      }
+
+      //如果当前节点存在子节点，需要递归解析
+      if (node.childNodes && node.childNodes.length > 0) {
+        this.compile(node)
+      }
+    })
+  }
+  //解析元素标签
+  compileElement(node) {
+    console.log('1111')
+  }
+  //解析文本节点
+  compileText(node) {
+    console.log('222')
+  }
+
+  /*---------------------------------工具方法-------------------------------*/
+  //转换为数组
   toArray(likeArray) {
     return [].slice.call(likeArray)
+  }
+  //是否为元素节点
+  isElementNode(node) {
+    return node.nodeType === 1
+  }
+  //是否为文本节点
+  isTextNode(node) {
+    return node.nodeType === 3
   }
 }
